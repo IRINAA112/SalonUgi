9887afc7a771f36062f05087b369c4c71580fc18
irina <alexairinamaria@gmail.com>
2025-02-20 12:56:43 +0200

    test

--- SalonUgi/Form1.Designer.cs
+++ SalonUgi/Form1.Designer.cs
@@ -34,22 +34,24 @@
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.GoBackPnl = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.GoBackPnl = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -89,10 +91,11 @@
             // 
             // pictureBox4
             // 
+            this.pictureBox4.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.pictureBox4.BackColor = System.Drawing.Color.PaleVioletRed;
             this.pictureBox4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.BackgroundImage")));
             this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox4.Location = new System.Drawing.Point(595, 48);
+            this.pictureBox4.Location = new System.Drawing.Point(54, 21);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(61, 276);
             this.pictureBox4.TabIndex = 3;
@@ -101,14 +104,13 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Transparent;
-            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.panel4);
             this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Controls.Add(this.pictureBox4);
             this.panel1.Controls.Add(this.pictureBox3);
             this.panel1.Controls.Add(this.pictureBox2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
@@ -117,84 +119,30 @@
             this.panel1.Size = new System.Drawing.Size(808, 329);
             this.panel1.TabIndex = 4;
             // 
-            // panel2
+            // label7
             // 
-            this.panel2.BackColor = System.Drawing.Color.Transparent;
-            this.panel2.Controls.Add(this.panel3);
-            this.panel2.Controls.Add(this.GoBackPnl);
-            this.panel2.Controls.Add(this.label1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(808, 183);
-            this.panel2.TabIndex = 5;
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Pink;
+            this.label7.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.label7.Location = new System.Drawing.Point(-6, 117);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(174, 40);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Nutty Tip";
             // 
-            // label1
+            // label6
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Pink;
-            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.label1.Font = new System.Drawing.Font("Harrington", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.DeepPink;
-            this.label1.Location = new System.Drawing.Point(101, 68);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(577, 73);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Choose a nail shape";
-            // 
-            // GoBackPnl
-            // 
-            this.GoBackPnl.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("GoBackPnl.BackgroundImage")));
-            this.GoBackPnl.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.GoBackPnl.Location = new System.Drawing.Point(28, 68);
-            this.GoBackPnl.Name = "GoBackPnl";
-            this.GoBackPnl.Size = new System.Drawing.Size(67, 73);
-            this.GoBackPnl.TabIndex = 2;
-            // 
-            // panel3
-            // 
-            this.panel3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel3.BackgroundImage")));
-            this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel3.Location = new System.Drawing.Point(684, 68);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(67, 73);
-            this.panel3.TabIndex = 3;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Pink;
-            this.label2.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.MediumVioletRed;
-            this.label2.Location = new System.Drawing.Point(81, 245);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(127, 40);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Cardi B";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Pink;
-            this.label3.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.MediumVioletRed;
-            this.label3.Location = new System.Drawing.Point(96, 285);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(97, 26);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "type shi";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Pink;
-            this.label4.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.MediumVioletRed;
-            this.label4.Location = new System.Drawing.Point(237, 102);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(0, 40);
-            this.label4.TabIndex = 6;
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Pink;
+            this.label6.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.label6.Location = new System.Drawing.Point(337, 39);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(239, 40);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Grave Grippers";
             // 
             // label5
             // 
@@ -210,29 +158,93 @@
             this.label5.Text = "Brick Tip";
             this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
-            // label6
+            // label4
             // 
-            this.label6.AutoSize = true;
-            this.label6.BackColor = System.Drawing.Color.Pink;
-            this.label6.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.Color.MediumVioletRed;
-            this.label6.Location = new System.Drawing.Point(337, 39);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(239, 40);
-            this.label6.TabIndex = 8;
-            this.label6.Text = "Grave Grippers";
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Pink;
+            this.label4.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.label4.Location = new System.Drawing.Point(237, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 40);
+            this.label4.TabIndex = 6;
             // 
-            // label7
+            // label3
             // 
-            this.label7.AutoSize = true;
-            this.label7.BackColor = System.Drawing.Color.Pink;
-            this.label7.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.ForeColor = System.Drawing.Color.MediumVioletRed;
-            this.label7.Location = new System.Drawing.Point(544, 129);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(174, 40);
-            this.label7.TabIndex = 9;
-            this.label7.Text = "Nutty Tip";
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Pink;
+            this.label3.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.label3.Location = new System.Drawing.Point(96, 285);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 26);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "type shi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Pink;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.label2.Location = new System.Drawing.Point(81, 245);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 40);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Cardi B";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.GoBackPnl);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(808, 183);
+            this.panel2.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            this.panel3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel3.BackgroundImage")));
+            this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel3.Location = new System.Drawing.Point(684, 68);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(67, 73);
+            this.panel3.TabIndex = 3;
+            // 
+            // GoBackPnl
+            // 
+            this.GoBackPnl.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("GoBackPnl.BackgroundImage")));
+            this.GoBackPnl.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.GoBackPnl.Location = new System.Drawing.Point(28, 68);
+            this.GoBackPnl.Name = "GoBackPnl";
+            this.GoBackPnl.Size = new System.Drawing.Size(67, 73);
+            this.GoBackPnl.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Pink;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Harrington", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DeepPink;
+            this.label1.Location = new System.Drawing.Point(101, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(577, 73);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Choose a nail shape";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.label7);
+            this.panel4.Controls.Add(this.pictureBox4);
+            this.panel4.Location = new System.Drawing.Point(582, 3);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(169, 335);
+            this.panel4.TabIndex = 10;
             // 
             // Form1
             // 
@@ -254,6 +266,8 @@
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -275,5 +289,6 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel4;
     }
 }
